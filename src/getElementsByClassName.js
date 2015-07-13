44e03c84cadf22dfa4a6e7763f47b28db99cfac2
Livvie Lin <livvie.lin@gmail.com>
2015-07-13 00:26:13 -0700

    Implemented getElementsByClassName using recursion on internal function, without using document.body

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+	
+	var result = [];
+	var nodes = document.childNodes;
+
+	var nodeLoop = function(nodeList) {
+		for (var i = 0; i < nodeList.length; i++) {
+			var node = nodeList[i];
+			var classes = node.classList;
+			if (classes) {
+				for (var j = 0; j < classes.length; j++) {
+					if (classes[j] === className) {
+						result.push(node);
+					}
+				}
+			}
+			if (node.childNodes) {
+				nodeLoop(node.childNodes);
+			}
+		}
+	}
+
+	nodeLoop(nodes);
+
+	return result;
+
 };
