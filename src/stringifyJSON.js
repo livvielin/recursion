4e9bc512f69bfcbd57a9b839f48d0d50fa85547f
Livvie Lin <livvie.lin@gmail.com>
2015-07-12 22:16:43 -0700

    Implemented for cases involving string, number, null, or boolean

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,21 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	if (typeof obj === "string") {
+		return '"' + obj + '"';
+	}
+
+	else if (typeof obj === "number" || obj === null || typeof obj === "boolean") {
+		return "" + obj;
+	}
+
+	else if (Array.isArray(obj)) {
+		//
+	}
+
+	else if (typeof obj === "object") {
+		//
+	}
+
 };
