f7a5af1860505d14a4d0877f6e67c26f33d4da77
Livvie Lin <livvie.lin@gmail.com>
2015-07-12 22:30:58 -0700

    Added partial implemtation for cases with object as input

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -27,7 +27,17 @@ var stringifyJSON = function(obj) {
 	}
 
 	else if (typeof obj === "object") {
-		//
+		var keys = Object.keys(obj);
+		if (keys[0] === undefined) {
+			return '{}';
+		}
+		for (var i = 0; i < keys.length; i++) {
+			var keyString = stringifyJSON(keys[i]);
+			var valString = stringifyJSON(obj[keys[i]]);
+			objResult.push(keyString + ':' + valString);
+		}
+
+		return '{' + objResult.join(',') + '}';
 	}
 
 };
