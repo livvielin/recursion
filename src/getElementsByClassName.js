b1bc94753a34ab9badfa020a4faeda590bc39a8a
Livvie Lin <livvie.lin@gmail.com>
2015-09-16 12:15:05 -0700

    Edited parse and get elements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,8 +4,9 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, node) {
 	
+	/*
 	var result = [];
 	var nodes = document.childNodes;
 
@@ -28,6 +29,20 @@ var getElementsByClassName = function(className) {
 
 	nodeLoop(nodes);
 
+	return result;
+	*/
+
+	var result = [];
+	node = node || document.body;
+	for (var i = 0; i < node.classList.length; i++) {
+		if (node.classList[i] === className) {
+			result.push(node);
+		} else if (node.childNodes) {
+			for (var j = 0; j < node.childNodes.length; j++) {
+				return getElementsByClassName(className, node.childNodes[j]);
+			}
+		}
+	}
 	return result;
 
 };
